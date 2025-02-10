99613f71ee12c5743f15f398ae109c240c814707
hazavi <kyyguevvara@gmail.com>
2025-02-10 20:21:37 +0100

    yessir

---
+++ EnerGize/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { ExercisesComponent } from './components/exercises/exercises.component';
+import { HomeComponent } from './components/home/home.component';
+import { PlansComponent } from './components/plans/plans.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default redirect to home
+  { path: 'home', component: HomeComponent }, // Home Page
+  { path: 'exercises', component: ExercisesComponent }, // Exercises Page
+  { path: 'plans', component: PlansComponent }, // Exercises Page
+];
